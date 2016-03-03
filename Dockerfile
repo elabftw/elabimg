efcc1780a186687a2f8b1d5372c17a29c6423999
NicolasCARPi <nicolas.carpi@curie.fr>
2016-03-03 12:06:22 +0100

    first working version of alpine

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM alpine:3.3
 MAINTAINER Nicolas CARPi <nicolas.carpi@curie.fr>
 
 # install nginx and php-fpm
-RUN apk add --update nginx openssl php php-pdo php-pdo_mysql php-fpm php-mysql php-gd php-curl git unzip supervisor && rm -rf /var/cache/apk/*
+RUN apk add --update nginx openssl php php-pdo php-pdo_mysql php-fpm php-mysql php-gd php-curl php-zip php-zlib php-json php-gettext git supervisor && rm -rf /var/cache/apk/*
 
 # elabftw
 #RUN git clone --depth 1 -b master https://github.com/elabftw/elabftw.git /elabftw
@@ -11,17 +11,18 @@ RUN git clone --depth 1 -b hypernext https://github.com/elabftw/elabftw.git /ela
 
 # only HTTPS
 EXPOSE 443
-# debug
+# for debug
 EXPOSE 9001
 
 # add files
-ADD ./nginx-https-443.conf /etc/nginx/
-ADD ./nginx-http-443.conf /etc/nginx/
-ADD ./supervisord.conf /etc/supervisord.conf
-ADD ./run.sh /run.sh
+COPY ./nginx.conf /etc/nginx/
+COPY ./https.conf /etc/nginx/
+COPY ./http.conf /etc/nginx/
+COPY ./supervisord.conf /etc/supervisord.conf
+COPY ./run.sh /run.sh
 
 # start
-CMD /run.sh
+ENTRYPOINT /run.sh
 
 # define mountable directories.
 VOLUME ["/elabftw/uploads"]
--- run.sh
+++ run.sh
@@ -1,36 +1,19 @@
 #!/bin/sh
-# elabftw-docker start script
+# elabftw-docker start script for alpine-linux base image
 
-# write config file from env var
-db_host=${DB_HOST}
-db_name=${DB_NAME:-elabftw}
-db_user=${DB_USER:-elabftw}
-db_password=${DB_PASSWORD}
-server_name=${SERVER_NAME:-localhost}
-disable_https=${DISABLE_HTTPS:-false}
-new_secret_key=$(php /elabftw/install/generateSecretKey.php)
-secret_key=${SECRET_KEY:-$new_secret_key}
+# get env values
+getEnv() {
+	db_host=${DB_HOST}
+	db_name=${DB_NAME:-elabftw}
+	db_user=${DB_USER:-elabftw}
+	db_password=${DB_PASSWORD}
+	server_name=${SERVER_NAME:-localhost}
+	disable_https=${DISABLE_HTTPS:-false}
+	secret_key=${SECRET_KEY}
+}
 
-cat << EOF > /elabftw/config.php
-<?php
-define('DB_HOST', '${db_host}');
-define('DB_NAME', '${db_name}');
-define('DB_USER', '${db_user}');
-define('DB_PASSWORD', '${db_password}');
-define('ELAB_ROOT', '/elabftw/');
-define('SECRET_KEY', '${secret_key}');
-EOF
-
-# remove the default config file
-rm /etc/nginx/nginx.conf
-
-# Switch http or https
-# false by default
-if ($disable_https); then
-    # activate an HTTP server listening on port 443
-    ln -s /etc/nginx/nginx-http-443.conf /etc/nginx/nginx.conf
-else
-    # generate self-signed certificates for nginx server
+# generate self-signed certificates for nginx server
+generateCerts() {
     mkdir -p /etc/nginx/certs
 
     if [ ! -f /etc/nginx/certs/server.crt ]; then
@@ -49,31 +32,71 @@ else
     if [ ! -f /etc/nginx/certs/dhparam.pem ]; then
         openssl dhparam -outform PEM -out /etc/nginx/certs/dhparam.pem 2048
     fi
+}
 
-    # activate an HTTPS server listening on port 443
-    ln -s /etc/nginx/nginx-https-443.conf /etc/nginx/nginx.conf
-fi
+nginxConf() {
+	# Switch http or https
+	# false by default
+	if ($disable_https); then
+		# activate an HTTP server listening on port 443
+		ln -s /etc/nginx/http.conf /etc/nginx/conf.d/elabftw.conf
+	else
+		generateCerts
+		# activate an HTTPS server listening on port 443
+		ln -s /etc/nginx/https.conf /etc/nginx/conf.d/elabftw.conf
+	fi
 
-# nginx config
-sed -i -e "s/localhost/$server_name/" /etc/nginx/nginx.conf
+	# fix the server name in nginx config
+	sed -i -e "s/localhost/$server_name/" /etc/nginx/conf.d/elabftw.conf
+}
 
-# php-fpm config
-sed -i -e "s/;daemonize\s*=\s*yes/daemonize = no/g" /etc/php/php-fpm.conf
-sed -i -e "s/;catch_workers_output\s*=\s*yes/catch_workers_output = yes/g" /etc/php/php-fpm.conf
-sed -i -e "s;listen = 127.0.0.1:9000;listen = /var/run/php-fpm.sock;g" /etc/php/php-fpm.conf
-sed -i -e "s/;listen.owner = nobody/listen.owner = nginx/g" /etc/php/php-fpm.conf
-sed -i -e "s/;listen.group = nobody/listen.group = nginx/g" /etc/php/php-fpm.conf
+phpfpmConf() {
+	# php-fpm config
+	sed -i -e "s/;daemonize\s*=\s*yes/daemonize = no/g" /etc/php/php-fpm.conf
+	sed -i -e "s/;catch_workers_output\s*=\s*yes/catch_workers_output = yes/g" /etc/php/php-fpm.conf
+	# use a unix socket
+	sed -i -e "s;listen = 127.0.0.1:9000;listen = /var/run/php-fpm.sock;g" /etc/php/php-fpm.conf
+	sed -i -e "s/;listen.owner = nobody/listen.owner = nginx/g" /etc/php/php-fpm.conf
+	sed -i -e "s/;listen.group = nobody/listen.group = nginx/g" /etc/php/php-fpm.conf
+}
 
-# php config
-sed -i -e "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" /etc/php/php.ini
-sed -i -e "s/upload_max_filesize\s*=\s*2M/upload_max_filesize = 100M/g" /etc/php/php.ini
-sed -i -e "s/post_max_size\s*=\s*8M/post_max_size = 100M/g" /etc/php/php.ini
+phpConf() {
+	# php config
+	sed -i -e "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" /etc/php/php.ini
+	sed -i -e "s/upload_max_filesize\s*=\s*2M/upload_max_filesize = 100M/g" /etc/php/php.ini
+	sed -i -e "s/post_max_size\s*=\s*8M/post_max_size = 100M/g" /etc/php/php.ini
+	# the sessions are stored in a separate dir
+	sed -i -e "s;session.save_path = \"/tmp\";session.save_path = \"/sessions\";g" /etc/php/php.ini
+	mkdir /sessions
+	chown nginx:nginx /sessions
+}
 
-# elabftw
-mkdir -p /elabftw/uploads/tmp
-chmod -R 755 /elabftw/uploads
-chown -R nginx:nginx /elabftw
-chmod -R u+x /elabftw/*
+elabftwConf() {
+	mkdir -p /elabftw/uploads/tmp
+	chmod -R 755 /elabftw/uploads
+	chown -R nginx:nginx /elabftw
+	chmod -R u+x /elabftw/*
+}
+
+writeConfigFile() {
+	# write config file from env var
+	config="<?php
+	define('DB_HOST', '${db_host}');
+	define('DB_NAME', '${db_name}');
+	define('DB_USER', '${db_user}');
+	define('DB_PASSWORD', '${db_password}');
+	define('ELAB_ROOT', '/elabftw/');
+	define('SECRET_KEY', '${secret_key}');"
+	echo $config > /elabftw/config.php
+}
+
+# script start
+getEnv
+nginxConf
+phpfpmConf
+phpConf
+elabftwConf
+writeConfigFile
 
 # start all the services
 /usr/bin/supervisord -c /etc/supervisord.conf -n
